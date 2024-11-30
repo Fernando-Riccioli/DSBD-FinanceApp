0ca8896fe4498d1953d88b190aeb83d63baaeb3d
Daniele Lucifora <111700564+danielux86@users.noreply.github.com>
2024-11-30 11:57:46 +0100

    Aggiunta Docker

---
+++ init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE utenti (
+    email VARCHAR(100) NOT NULL UNIQUE PRIMARY KEY,
+    ticker VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE data (
+    id VARCHAR(64) PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    ticker VARCHAR(5) NOT NULL,
+    valore FLOAT(10),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (email) REFERENCES utenti(email)
+);
